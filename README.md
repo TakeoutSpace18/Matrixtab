b9409f2c80cdce69f27f5ea2da300039f356fcd9
TakeoutSpace18 <u.pavel03@gmail.com>
2018-03-02 16:31:23 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Matrixtab
---
+++ firmware/ino/main/main.ino
@@ -0,0 +1,53 @@
+#include <Max72xxPanel.h>
+#include <Adafruit_GFX.h>
+#include <ArduinoJson.h>
+
+
+
+#define HORIZONTAL_MATRIX 4
+#define VERTCAL_MATRIX 2
+
+#define LOAD_PIN
+int CS_PIN = 0;
+#define CLK_PIN
+
+
+int brightness = 8; //Яркость матрицы
+String img = "00000000000000000000000000000000000100000001110000111000001001000011000000100100000001000010010000010000000001000011100000111100000100000000100000000100000001000001000000010000000001000000010000111000001111000011100000000100000000000000000000000000000000000000000000000000000000000000000000111100000110000011110000011000001000000010010000000100001001000011100000111000000010000001100000000100001001000001000000100100000001000010010000010000001001000011100000011000000100000001100000000000000000000000000000000000";
+int cur_symbol = 0;
+
+int horizontal_px = HORIZONTAL_MATRIX * 8;
+int vertical_px = VERTCAL_MATRIX * 8;
+int num_px = horizontal_px * vertical_px;
+
+
+Max72xxPanel matrix = Max72xxPanel(CS_PIN, HORIZONTAL_MATRIX, VERTCAL_MATRIX);
+
+
+void draw_img(String data)
+{
+    for (int y = 0; y <= vertical_px - 1; y++)
+    {
+        for (int x = 0; x <= horizontal_px -1; x++)
+        {
+            matrix.drawPixel(x, y, data[cur_symbol]);
+
+            cur_symbol++;
+        }
+        cur_symbol++;
+    }
+    matrix.write();
+}
+
+
+void setup()
+{
+matrix.setIntensity(brightness);
+draw_img(img);
+}
+
+void loop()
+{
+
+}
+
